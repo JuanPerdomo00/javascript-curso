496912368053a3c1c77f62de8c28072c77a84f7c
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-06-09 14:43:36 -0500

    first commit

---
+++ que_es_js/index.js
@@ -0,0 +1,13 @@
+console.log('Hola Mundo en javascript');
+
+// ! primera funcion en javascript
+
+suma = (a, b) => {
+	return a + b;
+}
+
+
+console.log(suma(3,3));
+
+console.log('primer programa en javascript');
+
